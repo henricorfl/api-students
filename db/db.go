48d0d249229a881f6eddd50b3b456e1b820d3d36
henricorfl <henrrfl@gmail.com>
2025-12-01 21:58:07 -0300

    Create dynamic student in DB

--- db/db.go
+++ db/db.go
@@ -12,11 +12,11 @@ import (
 
 type Student struct {
 	gorm.Model
-	Name   string
-	CPF    int
-	Email  string
-	Age    int
-	Active bool
+	Name   string `json:"name"`
+	CPF    int `json:"cpf"`
+	Email  string `json:"email"`
+	Age    int `json:"age"`
+	Active bool `json:"registration"`
 }
 
 func Init() *gorm.DB {
@@ -30,16 +30,8 @@ func Init() *gorm.DB {
 	return db
 }
 
-func AddStudent() {
+func AddStudent(student Student) {
 	db := Init()
-
-	student := Student{
-		Name:   "Rafael",
-		CPF:    12345678900,
-		Email:  "henrrfl@gmail.com",
-		Age:    32,
-		Active: true,
-	}
 
 	if result := db.Create(&student); result.Error != nil {
 		fmt.Println("Error to create student")
